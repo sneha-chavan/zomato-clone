63843f633703d60dfa8fd9ca1c3743140980563a
sneha-chavan <snehachavan115@gmail.com>
2020-10-09 02:28:21 +0530

    current location and cuisine search

--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -1,11 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './SearchBar.css'
+import zomato from '../../config/zomato';
 
 import LocationOnIcon from '@material-ui/icons/LocationOn';
 import SearchIcon from '@material-ui/icons/Search';
 import { FormControl, NativeSelect } from '@material-ui/core';
 
 function SearchBar() {
+    const [cuisine, setCuisine] = useState('');
+    const [nearByRestaurant, setNearByRestaurant] = useState([]);
+    const [operation, setOperation] = useState("Search");
+
+    // cuisine ='' , nearByReastaurant=[], operation ='search'
+
+    const handleSearch = (e) => {
+        e.preventDefault();
+        navigator.geolocation.getCurrentPosition(async (position) => {
+            setOperation("Fetching location....")
+            const lat = position.coords.latitude;
+            const lon = position.coords.longitude;
+            console.log("lat= " + lat + " lon= " + lon);
+
+            const url = await zomato
+                .get(`search?start=0&count=10&lat=${lat}&lon=${lon}&radius=5000000&cuisines=${cuisine}`)
+                .then(response => {
+                    setNearByRestaurant(response.data.restaurants)
+                })
+                .catch(error => {
+                    console.log(error);
+                });
+
+
+        })
+    }
+
+    console.log(nearByRestaurant);
     return (
         <div className="searchbar">
             {/* <h1 className="searchbar__title">zomato</h1> */}
@@ -30,9 +59,16 @@ function SearchBar() {
 
                 </NativeSelect>
                 </FormControl> */}
-                <input className="searchbar__input" type="text " placeholder="Search for restaurant, cuisine or a dish" />
+                <input
+                    className="searchbar__input"
+                    type="text "
+                    placeholder="Search for restaurant, cuisine or a dish"
+                    value={cuisine}
+                    onChange={e => setCuisine(e.target.value)}
+                />
 
                 <SearchIcon className="searchbar__searchIcon" />
+                <button value={operation} onClick={handleSearch}>Search</button>
             </div>
 
         </div>
